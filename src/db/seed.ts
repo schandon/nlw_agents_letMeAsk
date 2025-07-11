869e770d96b81fd23bb4925807a7bcfb842f79e2
Alexandre Souza Pereira <sp.alexandre26@gmail.com>
2025-07-11 16:09:17 -0300

    feat: updated a seed number of creating

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,13 +7,13 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
       with: {
-        questions: 20,
+        questions: 3,
       },
     },
   }
